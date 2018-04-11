59a217034c937b5b13ca45cf9f8dcc947fd56240
Abhishek Tiwari <tiwari.abhishek428@gmail.com>
2018-04-11 21:41:52 +0530

    Complete website

---
+++ js/inflate.js
@@ -0,0 +1,84 @@
+
+$(document).ready(function() {
+	$('#CEMENT').on('click',function() {
+		document.getElementById("r1c1").innerHTML = "<image src='images/jaypee.png' class='card-panel client-img'>";
+		document.getElementById("r1c2").innerHTML = "<image src='images/ultratech.png' class='card-panel client-img'>";
+		document.getElementById("r1c3").innerHTML = "<image src='images/birla-perfect.jpeg' class='card-panel client-img'>";
+		document.getElementById("r2c1").innerHTML = "<image src='images/acc.png' class='card-panel client-img'>";
+		document.getElementById("r2c2").innerHTML = "<image src='images/prism.png' class='card-panel client-img'>";
+		document.getElementById("r2c3").innerHTML = "<image src='images/jindal.png' class='card-panel client-img'>";
+		document.getElementById("r3c1").innerHTML = "<image src='images/kjs.jpeg' class='card-panel client-img'>";
+	});
+	$('#TMTBAR').on('click',function() {
+		document.getElementById("r1c1").innerHTML = "<image src='images/tata-tiscon.jpg' class='card-panel client-img'>";
+		document.getElementById("r1c2").innerHTML = "<image src='images/kamdhenu.jpg' class='card-panel client-img'>";
+		document.getElementById("r1c3").innerHTML = "<image src='images/radhe.jpeg' class='card-panel client-img'>";
+		document.getElementById("r2c1").innerHTML = "<image src='images/tara.jpeg' class='card-panel client-img'>";
+		document.getElementById("r2c2").innerHTML = "";
+		document.getElementById("r2c3").innerHTML = "";
+		document.getElementById("r3c1").innerHTML = "";
+	});
+	$('#TILES').on('click',function() {
+		document.getElementById("r1c1").innerHTML = "<image src='images/kajaria.jpeg' class='card-panel client-img'>";
+		document.getElementById("r1c2").innerHTML = "";
+		document.getElementById("r1c3").innerHTML = "";
+		document.getElementById("r2c1").innerHTML = "";
+		document.getElementById("r2c2").innerHTML = "";
+		document.getElementById("r2c3").innerHTML = "";
+		document.getElementById("r3c1").innerHTML = "";
+	});
+	$('#SANITARY').on('click',function() {
+		document.getElementById("r1c1").innerHTML = "<image src='images/hindware.jpg' class='card-panel client-img'>";
+		document.getElementById("r1c2").innerHTML = "<image src='images/cera.png' class='card-panel client-img'>";
+		document.getElementById("r1c3").innerHTML = "<image src='images/parryware.png' class='card-panel client-img'>";
+		document.getElementById("r2c1").innerHTML = "<image src='images/sonet.png' class='card-panel client-img'>";
+		document.getElementById("r2c2").innerHTML = "";
+		document.getElementById("r2c3").innerHTML = "";
+		document.getElementById("r3c1").innerHTML = "";
+	});
+	$('#CMTSHEETS').on('click',function() {
+		document.getElementById("r1c1").innerHTML = "<image src='images/jaypee.png' class='card-panel client-img'>";
+		document.getElementById("r1c2").innerHTML = "<image src='images/everest.png' class='card-panel client-img'>";
+		document.getElementById("r1c3").innerHTML = "<image src='images/charminar.png' class='card-panel client-img'>";
+		document.getElementById("r2c1").innerHTML = "";
+		document.getElementById("r2c2").innerHTML = "";
+		document.getElementById("r2c3").innerHTML = "";
+		document.getElementById("r3c1").innerHTML = "";
+	});
+	$('#GISHEETS').on('click',function() {
+		document.getElementById("r1c1").innerHTML = "<image src='images/tataShaktee.png' class='card-panel client-img'>";
+		document.getElementById("r1c2").innerHTML = "<image src='images/jsw.png' class='card-panel client-img'>";
+		document.getElementById("r1c3").innerHTML = "";
+		document.getElementById("r2c1").innerHTML = "";
+		document.getElementById("r2c2").innerHTML = "";
+		document.getElementById("r2c3").innerHTML = "";
+		document.getElementById("r3c1").innerHTML = "";
+	});
+	$('#PIPES').on('click',function() {
+		document.getElementById("r1c1").innerHTML = "<image src='images/astral-pipes.jpg' class='card-panel client-img'>";
+		document.getElementById("r1c2").innerHTML = "<image src='images/jindal.jpeg' class='card-panel client-img'>";
+		document.getElementById("r1c3").innerHTML = "";//<image src='' class='card-panel client-img'>";
+		document.getElementById("r2c1").innerHTML = "";//<image src='' class='card-panel client-img'>";
+		document.getElementById("r2c2").innerHTML = "";//<image src='' class='card-panel client-img'>";
+		document.getElementById("r2c3").innerHTML = "";
+		document.getElementById("r3c1").innerHTML = "";
+	});
+	$('#WATERTANKS').on('click',function() {
+		document.getElementById("r1c1").innerHTML = "<image src='images/sintex.png' class='card-panel client-img'>";
+		document.getElementById("r1c2").innerHTML = "<image src='images/supreme.png' class='card-panel client-img'>";
+		document.getElementById("r1c3").innerHTML = "<image src='images/topcon.png' class='card-panel client-img'>";
+		document.getElementById("r2c1").innerHTML = "";
+		document.getElementById("r2c2").innerHTML = "";
+		document.getElementById("r2c3").innerHTML = "";
+		document.getElementById("r3c1").innerHTML = "";
+	});
+	$('#PLYBOARDS').on('click',function() {
+		document.getElementById("r1c1").innerHTML = "<image src='images/centuryply.png' class='card-panel client-img'>";
+		document.getElementById("r1c2").innerHTML = "";//<image src='' class='card-panel client-img'>";
+		document.getElementById("r1c3").innerHTML = "";
+		document.getElementById("r2c1").innerHTML = "";
+		document.getElementById("r2c2").innerHTML = "";
+		document.getElementById("r2c3").innerHTML = "";
+		document.getElementById("r3c1").innerHTML = "";
+	});
+});
